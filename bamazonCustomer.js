f9f4f41748bef0f80c7a9052bab35d1ed5bbe033
Faisal Mohamed <faisalmohamed@Faisals-MacBook-Pro.local>
2017-05-08 10:45:30 -0400

    finished first level

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var blank = ('\n ===========================================') 
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: '3306',
@@ -23,8 +24,102 @@ function startShop() {
 		}
 
 	).then(function(answer) {
-		console.log('\n ===========================================');
+		console.log(blank);
 		console.log('\nWelcome', answer.name, '\nThese are today\'s products...\n');
 		productsList(); 
 	})
 }
+
+function itemList() {
+	connection.query('SELECT * FROM products', function(err, results) {
+		if (err) throw err;
+		for (var i=0; i < results.length; i++) {
+			console.log(results[i].id + '\nProduct: ' + results[i].product_name + '\nPrice: $' + results[i].price + '.00');
+			console.log(blank)
+		}
+		buyingPrompt();
+	})
+}
+
+// asking the customer what they would like to purchase
+function buyingPrompt() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item',
+			message: 'Which product would you like to buy (id)?'
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many would you like?'
+		}
+	]).then(function(answer) {
+		connection.query('SELECT * FROM products WHERE id = ?', [answer.item], function(err, result) {
+			total = (result[0].price * answer.quantity).toFixed[2];
+			dept = result[0].department_name;
+			if (answer.quantity > result[0].stock_quantity) {
+				console.log('Insufficient quantity! Please shop again another time.');
+				endConnection();
+			} else {
+				connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?', [answer.quantity, answer.item], function(err, result) {
+					connection.query('UPDATE department_name SET total_sales = total_sales + ? WHERE department_name = ?' [total, dept], function(err, result) {
+						console.log('Your total for today is $' + total + '\nThanks for shopping!');
+						endConnection();
+					});
+				});
+			}
+		})
+	})
+
+	}
+
+	// end connection function
+	function endConnection() {
+		connection.end(function(err) {
+			if (err) throw err;
+		})
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
